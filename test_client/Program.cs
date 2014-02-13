8f722d674dcfd8c09ad9130c235e6c36bc8ef09f
David Keymer <david.keymer@stfc.ac.uk>
2014-02-13 15:37:53 +0000

    #270 added main solution and test client
    Former-commit-id: 0141d5d83fcbe5223885b78db37c5d59e02bf1f6

---
+++ test_client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PSI.EpicsClient2;
+
+namespace test_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EpicsClient client = new EpicsClient();
+            EpicsChannel<int> test_channel_1 = client.CreateChannel<int>("NDW1033:dpk62:SIMPLE:VALUE1");
+            int PV_value = test_channel_1.Get<int>();
+            Console.WriteLine(PV_value);
+            test_channel_1.Put<int>(2);
+        }
+    }
+}
