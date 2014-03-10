f1420884acbf51fae081296cee7992e989633a68
Matt Clarke <matt.clarke@stfc.ac.uk>
2014-03-10 15:32:13 +0000

    Refs #306. Fixed a bug where it would try to send a beacon message of the wrong length because it was trying to include the IPV6 address!
    Former-commit-id: e67609f93ae78bbd732f33a481c122f4c28f8b25

--- server/CABeacon.cs
+++ server/CABeacon.cs
@@ -21,7 +21,7 @@ namespace CaSharpServer
         {
             endPoint = new IPEndPoint(IPAddress.Broadcast, udpPort);
             if (server.ServerAddress == IPAddress.Any)
-                serverIps.AddRange(Dns.GetHostAddresses(Dns.GetHostName()).Where(row=>!row.IsIPv6LinkLocal && !row.IsIPv6Multicast && !row.IsIPv6SiteLocal));
+                serverIps.AddRange(Dns.GetHostAddresses(Dns.GetHostName()).Where(row => !row.IsIPv6LinkLocal && !row.IsIPv6Multicast && !row.IsIPv6SiteLocal && row.AddressFamily == AddressFamily.InterNetwork));
             else
                 serverIps.Add(server.ServerAddress);
 
